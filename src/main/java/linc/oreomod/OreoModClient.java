a2289bbe9a5a7afe149609edc5a674d5ea3c77d9
leenc <leenchello@gmail.com>
2024-12-14 19:50:04 +0100

    Initial commit

---
+++ src/main/java/linc/oreomod/OreoModClient.java
@@ -0,0 +1,10 @@
+package linc.oreomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OreoModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/linc/oreomod/OreoModDataGenerator.java
@@ -0,0 +1,11 @@
+package linc.oreomod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OreoModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
